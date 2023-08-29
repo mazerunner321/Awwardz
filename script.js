c3147932c33bfff6a8893228b508efb0b52b0a01
mazerunner321 <prajwalhg321@gmail.com>
2023-08-29 19:46:48 +0530

    final

--- script.js
+++ script.js
@@ -63,3 +63,32 @@ function firstPageAnim() {
     });
 }
 firstPageAnim();
+
+//Image
+document.querySelectorAll(".elem").forEach((elem) => {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      duration: 0.5,
+      ease: Power3,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    // console.log(elem.getBoundingClientRect());
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot),
+    });
+  });
+});
